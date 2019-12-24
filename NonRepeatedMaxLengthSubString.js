87e27e3b71df011bbd270a22500b09936c58d814
Sreekanth <seeku.sreekanth@gmail.com>
2019-12-24 11:47:49 +0530

    Update NonRepeatedMaxLengthSubString.js

--- NonRepeatedMaxLengthSubString.js
+++ NonRepeatedMaxLengthSubString.js
@@ -1,33 +1,23 @@
-let str = "BADDAFGDGEDADE";
-let strMap = {};
-let count = 0;
-let maxCount = 0;
-let startIndex = 0;
-let chars = "";
-
-function getNonRepeatedMaxLengthSubString() {
+function getNonRepeatedMaxLengthSubString(str) {
+  let strMap = {};
+  let count = 0;
+  let maxCount = 0;
+  let startIndex = 0;
+  let chars = "";
   for (let i = 0; i < str.length; i++) {
     const prevIndex = strMap[str[i]];
     if (prevIndex === undefined || i - count > prevIndex) {
       count++;
-      chars = str.substr(startIndex, maxCount);
     } else {
       const subStringCount = i - prevIndex;
-      if (subStringCount >= maxCount) {
-        startIndex = prevIndex + 1;
-      }
+      startIndex = prevIndex + 1;
       count = subStringCount;
     }
     strMap[str[i]] = i;
-    if (count > maxCount) {
+    if (count >= maxCount) {
       maxCount = count;
+      chars = str.substr(startIndex, maxCount);
     }
   }
   return { maxCnt: maxCount, NRChars: chars };
 }
-const { maxCnt, NRChars } = getNonRepeatedMaxLengthSubString();
-console.log(
-  "Max non repeated count is `%d`, and the string is %s",
-  maxCnt,
-  NRChars
-);
