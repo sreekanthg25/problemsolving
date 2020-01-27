40bd166cc8960aee077cf15914e53f4180f15d70
Sreekanth <seeku.sreekanth@gmail.com>
2020-01-27 15:16:46 +0530

    Update differencyBy.js

--- utils/differencyBy.js
+++ utils/differencyBy.js
@@ -1,8 +1,13 @@
-function differenceBy(array, values, fn) {
-    return array.reduce((acc, value) => {
-       const sourceVal = fn(value);
-       const valueExistsInSource = values.some(val => sourceVal === fn(val));
-       if(!valueExistsInSource) { acc.push(value); }
-       return acc;
-    },[])
+function differenceBy(source = [], values, fn, map) {
+  const isSourceArray = Array.isArray(source);
+  const iterator = !isSourceArray ? Object.keys[source] : source;
+  return iterator.reduce((acc, value) => {
+    const isFunction = typeof fn === 'function';
+    const sourceVal = isFunction ? fn(isSourceArray ? value : source[value]) : value;
+    const valueExists = values.some(val => sourceVal === (isFunction ? fn(val) : val));
+    if(!valueExists) {
+      acc.push(typeof map === 'function' ? map(value) : value);
+    }
+    return acc;
+  },[]);
 }
